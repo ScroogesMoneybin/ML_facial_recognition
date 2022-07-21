b108e9407d14c8e8b8f0c23abf62c2daef0cfda9
Nick <kammer@gmail.com>
2022-07-21 15:03:30 -0400

    update

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ const signin=require("./controllers/signin.js");
 const getprofile=require("./controllers/getprofile.js");
 const image=require("./controllers/image.js");
 
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = 0; 
 const pg_db=knex({
     client: 'pg',
     connection: {
